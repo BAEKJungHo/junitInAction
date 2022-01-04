30cf5812f0d4de63f75a4c9ddba7ccef90c10040
BAEKJungHo <designjava@naver.com>
2022-01-04 22:23:02 +0900

    Create Project

---
+++ src/src/main/java/junit/in/action/JunitinactionApplication.java
@@ -0,0 +1,13 @@
+package junit.in.action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitinactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitinactionApplication.class, args);
+    }
+
+}
